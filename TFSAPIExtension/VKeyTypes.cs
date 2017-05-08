c411d968fe36c4eb33629f06cefae33519f3a2d9
haoziyanran <75821402@qq.com>
2017-05-08 16:06:07 +0800

    创建VKeyTypes.cs文件
    1.VModel模型的Key类型，RS,SFS,SSFS,DS,TestCase(STS,SITS,SSITS,UTS)

---
+++ TFSAPIExtension/VKeyTypes.cs
@@ -0,0 +1,23 @@
+namespace TFSAPIExtension
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public sealed class VKeyTypes
+    {
+        public const string RS = "RS";
+        public const string SFS = "SFS";
+        public const string SSFS = "SSFS";
+        public const string DS = "DS";
+        public const string STS = "STS";
+        public const string SITS = "SITS";
+        public const string SSITS = "SSITS";
+        public const string UTS = "UTS";
+        public const string TestCase = "Test Case";
+    }
+}
