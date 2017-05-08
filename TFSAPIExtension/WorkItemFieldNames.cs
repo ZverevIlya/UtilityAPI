8e62adb8afb724256fdf325920d43de1befa0172
haoziyanran <75821402@qq.com>
2017-05-08 16:23:31 +0800

    创建WorkItemFieldNames.cs文件
    1.WorkItem字段名称

---
+++ TFSAPIExtension/WorkItemFieldNames.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TFSAPIExtension
+{
+    public static class WorkItemFieldNames
+    {
+        public const string Title = "Title";
+        public const string State = "State";
+        public const string Rev = "Rev";
+        public const string ChangedBy = "Changed By";
+        public const string Reason = "Reason";
+        public const string AssignedTo = "Assigned To";
+        public const string WorkItemType = "Work Item Type";
+        public const string CreatedDate = "Created Date";
+        public const string CreatedBy = "Created By";
+        public const string Description = "Description";
+        public const string History = "History";
+        public const string RelatedLinkCount = "Related Link Count";
+        public const string StateChangeDate = "State Change Date";
+        public const string ActivatedDate = "Activated Date";
+        public const string ActivatedBy = "Activated By";
+        public const string ClosedDate = "Closed Date";
+        public const string ClosedBy = "Closed By";
+        public const string IntegrationBuild = "Integration Build";
+        public const string Issue = "Issue";
+        public const string RemainingWork = "Remaining Work";
+        public const string CompletedWork = "Completed Work";
+        public const string StartDate = "Start Date";
+        public const string FinishDate = "Finish Date";
+        public const string IterationPath = "Iteration Path";
+        public const string IterationID = "Iteration ID";
+        public const string Rank = "Rank";
+        public const string ExitCriteria = "Exit Criteria";
+        public const string Discipline = "Discipline";
+        public const string BaselineWork = "Baseline Work";
+        public const string TaskHierarchy = "Task Hierarchy";
+        public const string ExternalLinkCount = "External Link Count";
+        public const string TeamProject = "Team Project";
+        public const string CodeFile = "CodeFile";
+        public const string CodeStartLine = "CodeStartLine";
+        public const string CodeEndLine = "CodeEndLine";
+        public const string CodeProject = "CodeProject";
+        public const string CodeMember = "CodeMember";
+        public const string CodeSnippet = "CodeSnippet";
+        public const string HyperlinkCount = "Hyperlink Count";
+        public const string AttachedFileCount = "Attached File Count";
+        public const string CodeItemType = "Code Item Type";
+        public const string CodePriority = "CodePriority";
+        public const string CodeDescription = "CodeDescription";
+        public const string CodeNamespace = "CodeNamespace";
+        public const string CodeClass = "CodeClass";
+        public const string CodeSuggestedResolution = "CodeSuggestedResolution";
+        public const string NodeName = "Node Name";
+        public const string AreaPath = "Area Path";
+        public const string RevisedDate = "Revised Date";
+        public const string ChangedDate = "Changed Date";
+        public const string ID = "ID";
+        public const string AreaID = "Area ID";
+        public const string AuthorizedAs = "Authorized As";
+        public const string ExpectFixedBranch = "Expect Fixed Branch";
+    }
+}
